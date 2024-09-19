3ea6f4bdeb31bd948eea34a686067dac07fdf533
BigJermaine <113020989+bigjermaine@users.noreply.github.com>
2024-09-19 06:15:32 +0100

    Initial Commit

---
+++ PlanTrip/PlanTripApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanTripApp.swift
+//  PlanTrip
+//
+//  Created by MacBook AIR on 19/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlanTripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
